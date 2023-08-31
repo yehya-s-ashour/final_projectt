96c7e58bd548fc5772af1c17e12c21593e70936c
Mohammed Hasaballah <mhasaballah21@gmail.com>
2023-08-31 16:02:09 +0300

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:final_projectt/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'Screens/main_screen.dart';
 import 'Screens/splash_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(fontFamily: 'Iphone'),
-    home: const SplashScreen(),
+    home: const MainPage(),
   ));
 }
