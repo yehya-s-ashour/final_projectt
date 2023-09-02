5faec6c4d7fcc8f9ee48f58b5b52bcef46a9f717
Ahmad Herzalla <ahmadakramherzalla@gmail.com>
2023-09-03 00:56:48 +0300

    home view

--- lib/core/util/constants/colors.dart
+++ lib/core/util/constants/colors.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xff0c44fd);
 Color scondaryColor = const Color.fromARGB(255, 206, 168, 251);
+Color backGroundColor = const Color(0xffF7F6FF);
+Color boxColor = const Color(0xffFFFFFF);
+Color shadowColor = const Color(0xffEFEEFC);
+Color inboxColor = const Color(0xffFA3A57);
+Color pendingColor = const Color(0xffFFE120);
+Color inProgressColor = const Color(0xff6589FF);
+Color completedColor = const Color(0xff77D16F);
