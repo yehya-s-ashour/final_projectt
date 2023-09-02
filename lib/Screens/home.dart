2feeef580344283edbd2c4d9b664a30009d5fefb
Ahmad Herzalla <ahmadakramherzalla@gmail.com>
2023-09-02 19:33:33 +0300

    home_screen

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,3 +1,7 @@
+import 'package:final_projectt/core/util/constants/colors.dart';
+import 'package:final_projectt/core/util/constants/configuration.dart';
+import 'package:final_projectt/core/widgets/card.dart';
+import 'package:final_projectt/core/widgets/custom_box.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ExpansionTileController controller = ExpansionTileController();
   double xoffset = 0;
   double yoffset = 0;
   double scalefactor = 1;
@@ -22,50 +27,180 @@ class _HomeScreenState extends State<HomeScreen> {
         ..scale(scalefactor),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(isdraweropen ? 20 : 0),
-        color: Colors.white,
+        color: backGroundColor,
       ),
       child: SingleChildScrollView(
-          child: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                isdraweropen
-                    ? IconButton(
-                        onPressed: () {
-                          setState(() {
-                            xoffset = 0;
-                            yoffset = 0;
-                            scalefactor = 1;
-                            isdraweropen = false;
-                          });
-                        },
-                        icon: const Icon(Icons.arrow_back_ios))
-                    : IconButton(
-                        icon: const Icon(Icons.menu),
-                        onPressed: () {
-                          setState(() {
-                            xoffset = 320;
-                            yoffset = 90;
-                            // the next line mean: home screen should be 80% of the screen before
-                            scalefactor = 0.8;
-                            isdraweropen = true;
-                          });
-                        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  isdraweropen
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              xoffset = 0;
+                              yoffset = 0;
+                              scalefactor = 1;
+                              isdraweropen = false;
+                            });
+                          },
+                          icon: const Icon(Icons.arrow_back_ios))
+                      : IconButton(
+                          icon: const Icon(Icons.menu),
+                          onPressed: () {
+                            setState(() {
+                              xoffset = 320;
+                              yoffset = 90;
+                              // the next line mean: home screen should be 80% of the screen before
+                              scalefactor = 0.8;
+                              isdraweropen = true;
+                            });
+                          },
+                        ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('images/person.jpg'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                    size: 24.0,
+                  ),
+                  filled: true,
+                  fillColor: boxColor,
+                  hintText: "Search",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: backGroundColor),
+                      borderRadius:
+                          BorderRadius.circular(40) // Border color when enabled
                       ),
-                const CircleAvatar(
-                  backgroundImage: AssetImage('images/person.jpg'),
                 ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomBox(color: inboxColor, number: "9", status: "Inbox"),
+                CustomBox(color: pendingColor, number: "9", status: "Pending")
               ],
             ),
-          ),
-        ],
-      )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomBox(
+                    color: inProgressColor, number: "9", status: "In progress"),
+                CustomBox(
+                    color: completedColor, number: "9", status: "Completed")
+              ],
+            ),
+            ExpansionTile(
+              textColor: const Color(0xff272727),
+              tilePadding: const EdgeInsets.symmetric(horizontal: 16),
+              initiallyExpanded: true,
+              title: const Text(
+                'Official Organization',
+                style: TextStyle(fontSize: 20),
+              ),
+              children: <Widget>[myCustomCard()],
+            ),
+            ExpansionTile(
+              textColor: const Color(0xff272727),
+              tilePadding: const EdgeInsets.symmetric(horizontal: 16),
+              initiallyExpanded: false,
+              title: const Text(
+                'NGOs',
+                style: TextStyle(fontSize: 20),
+              ),
+              children: <Widget>[myCustomCard()],
+            ),
+            ExpansionTile(
+              textColor: const Color(0xff272727),
+              tilePadding: const EdgeInsets.symmetric(horizontal: 16),
+              initiallyExpanded: true,
+              title: const Text(
+                'Others ',
+                style: TextStyle(fontSize: 20),
+              ),
+              children: <Widget>[myCustomCard()],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Tags",
+                style: TextStyle(fontSize: 20, color: Color(0xff272727)),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  color: boxColor,
+                  borderRadius: BorderRadiusDirectional.circular(16)),
+              child: Wrap(spacing: 16, children: [
+                TextButton(
+                    style: tagButtonStyle,
+                    onPressed: () {},
+                    child: Text("All Tags")),
+                TextButton(
+                    style: tagButtonStyle,
+                    onPressed: () {},
+                    child: Text("#Urgent")),
+                TextButton(
+                    style: tagButtonStyle,
+                    onPressed: () {},
+                    child: Text("#Egyptian Military ")),
+                TextButton(
+                    style: tagButtonStyle,
+                    onPressed: () {},
+                    child: Text("#New")),
+              ]),
+            ),
+            Container(
+              color: boxColor,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                          color: const Color(0xff6589FF),
+                          borderRadius: BorderRadiusDirectional.circular(12)),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      "New Inbox",
+                      style: TextStyle(color: Color(0xff6589FF), fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/core/util/constants/configuration.dart
+++ lib/core/util/constants/configuration.dart
@@ -9,3 +9,16 @@ List<Map> drawerItem = [
 
 //------------------------URLs------------------------
 const String baseUrl = 'https://palmail.gsgtt.tech/api';
+
+///-----------tag button style-----------
+ButtonStyle tagButtonStyle = ButtonStyle(
+  backgroundColor: const MaterialStatePropertyAll(Color(0xffE6E6E6)),
+  textStyle: const MaterialStatePropertyAll(
+      TextStyle(fontSize: 14, color: Color(0xff7C7C7C))),
+  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+    RoundedRectangleBorder(
+      borderRadius:
+          BorderRadius.circular(16.0), // Adjust the border radius as needed
+    ),
+  ),
+);
