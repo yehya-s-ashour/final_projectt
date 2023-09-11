73e0d4c3ebcfa353e07a9e5039c53f6d9df6a2b3
yehya-s-ashour <yehya.saad.ashour@gmail.com>
2023-09-11 12:12:59 +0300

    return to main

---
+++ lib/core/widgets/date_picker.dart
@@ -0,0 +1,132 @@
+import 'dart:convert';
+
+import 'package:final_projectt/core/util/constants/colors.dart';
+import 'package:final_projectt/providers/new_inbox_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CustomDatePicker extends StatefulWidget {
+  final bool? isLined;
+  CustomDatePicker({this.isLined = true});
+  @override
+  State<CustomDatePicker> createState() => _CustomDatePickerState();
+}
+
+class _CustomDatePickerState extends State<CustomDatePicker> {
+  DateTime date = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    int year = date.year;
+    int today = date.day;
+    dynamic month = getMonth(date);
+    dynamic todayName = getDay(date);
+
+    return Column(
+      children: [
+        Theme(
+          data: Theme.of(context).copyWith(
+            dividerColor: Colors.transparent,
+          ),
+          child: ExpansionTile(
+            trailing: AnimatedContainer(
+              duration: Duration(milliseconds: 400),
+              curve: Curves.linear,
+              width: Provider.of<NewInboxProvider>(context).isDatePickerOpened
+                  ? 35
+                  : 20,
+              child: Center(
+                child: Icon(
+                  !Provider.of<NewInboxProvider>(context).isDatePickerOpened
+                      ? Icons.arrow_forward_ios_rounded
+                      : Icons.keyboard_arrow_down_rounded,
+                  size:
+                      !Provider.of<NewInboxProvider>(context).isDatePickerOpened
+                          ? 22
+                          : 38,
+                  color:
+                      !Provider.of<NewInboxProvider>(context).isDatePickerOpened
+                          ? Colors.grey
+                          : primaryColor,
+                ),
+              ),
+            ),
+            onExpansionChanged: (value) {
+              setState(() {
+                Provider.of<NewInboxProvider>(context, listen: false)
+                    .setIsDatePickerOpened(value);
+              });
+            },
+            textColor: Color(0xff272727),
+            tilePadding: EdgeInsets.symmetric(horizontal: 22),
+            initiallyExpanded: false,
+            leading: Icon(
+              Icons.calendar_month_rounded,
+              color: Colors.red,
+              size: 23,
+            ),
+            childrenPadding: EdgeInsets.zero,
+            title: SizedBox(
+              width: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Date',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Iphone',
+                        fontSize: 19),
+                  ),
+                  Text(
+                    '$todayName, $month $today, $year',
+                    style: TextStyle(
+                        color: primaryColor,
+                        fontFamily: 'Iphone',
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            //
+            children: <Widget>[
+              CalendarDatePicker(
+                initialDate: date,
+                firstDate: DateTime(1900, 1, 1),
+                lastDate: DateTime(2100, 1, 1),
+                onDateChanged: (DateTime newdate) {
+                  setState(() {
+                    date = newdate;
+                    Provider.of<NewInboxProvider>(context, listen: false)
+                        .setDate(newdate);
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        widget.isLined!
+            ? Container(
+                margin: EdgeInsetsDirectional.only(start: 10, end: 10),
+                width: MediaQuery.sizeOf(context).width,
+                height: 1,
+                color: Colors.grey.shade300,
+              )
+            : SizedBox(),
+      ],
+    );
+  }
+}
+
+getDay(DateTime date) {
+  dynamic dayData =
+      '{ "1" : "Mon", "2" : "Tue", "3" : "Wed", "4" : "Thur", "5" : "Fri", "6" : "Sat", "7" : "Sun" }';
+  return json.decode(dayData)['${date.weekday}'];
+}
+
+getMonth(DateTime date) {
+  dynamic monthData =
+      '{ "1" : "Jan", "2" : "Feb", "3" : "Mar", "4" : "Apr", "5" : "May", "6" : "June", "7" : "Jul", "8" : "Aug", "9" : "Sep", "10" : "Oct", "11" : "Nov", "12" : "Dec" }';
+  return json.decode(monthData)['${date.month}'];
+}
---
+++ lib/core/widgets/senders_bottom_sheet.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/material.dart';
+import 'package:final_projectt/core/services/new_inbox_controller.dart';
+import 'package:final_projectt/core/util/constants/colors.dart';
+import 'package:final_projectt/models/sender_model.dart';
+
+class SendersBottomSheet extends StatefulWidget {
+  const SendersBottomSheet({super.key});
+
+  @override
+  State<SendersBottomSheet> createState() => _SendersBottomSheetState();
+}
+
+class _SendersBottomSheetState extends State<SendersBottomSheet> {
+  TextEditingController searchTextField = TextEditingController();
+  late Future<Senders> senders;
+  late Map<String, List<String>> searchMap = {};
+  dynamic sendersData;
+  late List<String> result = [];
+
+  @override
+  void initState() {
+    senders = getSenders();
+    searchSenders();
+    super.initState();
+  }
+
+  void searchSenders() {
+    // Define the map
+    Map<int, List<String>> data = {
+      80: ["reema", "04562"],
+      44: ["YazanCS", "0538844"],
+      // ... (other key-value pairs)
+      5: ["Yazz Hudson", "(617) 819-4825"],
+      6: ["Tyreek Hudson", "(617) 819-4825"]
+    };
+
+    // Substring to search for (convert to lowercase)
+    String substring = "YAZ".toLowerCase();
+
+    // Initialize a list to store matching key-value pairs
+    List<MapEntry<int, List<String>>> matchingPairs = [];
+
+    // Iterate through the map
+    data.forEach((key, valueList) {
+      for (String stringValue in valueList) {
+        // Convert the value to lowercase before checking
+        if (stringValue.toLowerCase().contains(substring)) {
+          matchingPairs.add(MapEntry(key, valueList));
+          break; // Stop searching within this value list once a match is found
+        }
+      }
+    });
+
+    // Print the matching key-value pairs
+    for (MapEntry<int, List<String>> entry in matchingPairs) {
+      print("Key: ${entry.key}, Value: ${entry.value}");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsDirectional.only(top: 25, start: 20, end: 20),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height - 150,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 270,
+                  child: TextField(
+                    controller: searchTextField,
+                    // onChanged: (value) {
+                    //    searchSenders(sendersData, searchTextField.text);
+                    // },
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search_rounded),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          searchTextField.clear();
+                        },
+                        icon: Icon(Icons.cancel),
+                      ),
+                      filled: true,
+                      fillColor: Colors.black.withOpacity(0.05),
+                      contentPadding: EdgeInsets.all(15),
+                      hintText: "Search ...",
+                      hintStyle:
+                          const TextStyle(color: Colors.grey, fontSize: 19),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: backGroundColor),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: backGroundColor),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () => Navigator.pop(context),
+                  child: Text(
+                    'cancel',
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 23,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsetsDirectional.only(top: 10.0),
+                child: FutureBuilder(
+                  future: senders,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    } else if (!snapshot.hasData) {
+                      return Center(
+                        child: Text('No data available.'),
+                      );
+                    }
+
+                    sendersData = snapshot.data as Senders;
+                    final categorizedSenders = categorizeSenders(sendersData);
+
+                    return ListView.separated(
+                      separatorBuilder: (context, index) {
+                        return SizedBox(
+                          height: 10,
+                        );
+                      },
+                      physics: BouncingScrollPhysics(),
+                      itemCount: categorizedSenders.length,
+                      itemBuilder: (context, sectionIndex) {
+                        final category =
+                            categorizedSenders.keys.elementAt(sectionIndex);
+                        final categorySenders = categorizedSenders[category]!;
+                        return Column(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.only(
+                                    top: 20.0,
+                                    start: 5,
+                                  ),
+                                  child: Text(
+                                    category,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 1,
+                                  color: Colors.grey.shade300,
+                                )
+                              ],
+                            ),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: categorySenders.length,
+                              itemBuilder: (context, itemIndex) {
+                                final sender = categorySenders[itemIndex];
+
+                                return ListTile(
+                                  onTap: () {
+                                    setState(() {
+                                      Navigator.pop(context, sender);
+                                    });
+                                  },
+                                  leading: Icon(Icons.person_3_outlined),
+                                  title: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        sender.name,
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        height: 8,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.phone,
+                                            size: 20,
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            sender.mobile,
+                                            style: TextStyle(fontSize: 17),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // searchSenders(Senders sendersData, String searchTerm) {
+  //   result.clear();
+  //   for (int i = 0; i < sendersData.data.length; i++) {
+  //     final sender = sendersData.data[i];
+  //     final senderName = sender.name;
+  //     final senderMobile = sender.mobile;
+
+  //     if (senderMobile.contains(searchTerm) ||
+  //         senderName.contains(searchTerm)) {
+  //       result.addAll([senderName, senderMobile]);
+  //     }
+  //   }
+  // }
+}
+
+Map<String, List<SingleSender>> categorizeSenders(Senders sendersData) {
+  final categorizedSenders = <String, List<SingleSender>>{};
+
+  for (final sender in sendersData.data) {
+    final category = sender.category.name;
+    if (!categorizedSenders.containsKey(category)) {
+      categorizedSenders[category] = <SingleSender>[];
+    }
+    categorizedSenders[category]!.add(sender);
+  }
+
+  return categorizedSenders;
+}
